fc811d8eeb6900b705c0e129ed75f2ba1559870c
kokwong <kok.wong@gmail.com>
2014-04-27 14:32:59 +0800

    Format the comments according to the 80 characters per line style

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,8 @@
 ## Git Profile: https://github.com/kokwong
 
 
-## makeCacheMatrix is a reference to function which has already loaded with matrix X content into it
+## makeCacheMatrix is a reference to function which has already loaded with 
+## matrix X content into it
 
 makeCacheMatrix <- function(x = matrix()) {
         solved <- NULL
@@ -19,7 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve is a reference to function which can generate the Inverse of a Matrix for the first time and look up the cached symbol table for subsequent executions
+## cacheSolve is a reference to function which can generate the Inverse of a 
+## Matrix for the first time and look up the cached symbol table for subsequent
+## executions
 
 cacheSolve <- function(x, ...) {
         solved <- x$getsolved()
